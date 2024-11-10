99d9d42f16bb7a8e23d8251de4f6bfcb320fee2d
Palal <moh.naufal1987@gmail.com>
2024-11-11 04:39:58 +0700

    projek master plan

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:master_plan/models/plan.dart';
+import 'package:master_plan/provider/plan_provider.dart';
+import 'package:master_plan/views/plan_screen.dart';
+
+void main() => runApp(const MasterPlanApp());
+
+class MasterPlanApp extends StatelessWidget {
+  const MasterPlanApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: const PlanCreatorScreen(),
+    );
+  }
+}
+
+class PlanCreatorScreen extends StatelessWidget {
+  const PlanCreatorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PlanProvider(
+      notifier: ValueNotifier<List<Plan>>([]),  
+      child: const PlanScreen(plan: Plan(name: 'My Plan', tasks: [])), 
+    );
+  }
+}
